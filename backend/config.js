f4b347b4a33df2fa44d9cf77dedbaaa1dab449eb
SupunCharuka <supuncharuka.dev@gmail.com>
2025-11-22 13:20:02 +0530

    feat: initialize frontend with Vite and React setup

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 3000;
+export const PORT = 5000;
 
 export const mongoDBURL = "mongodb+srv://root:lush@lush-task.21n3h3m.mongodb.net/lush-task?appName=lush-task";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 
 
